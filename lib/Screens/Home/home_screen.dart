bd6dd4979e311711ca79e84e3204d56e6ba6e2e3
Abdul Momen <abdulmomen130@gmail.com>
2022-01-22 17:53:58 +0600

    Added list tile

--- lib/Screens/Home/home_screen.dart
+++ lib/Screens/Home/home_screen.dart
@@ -1,20 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:swipe_words/constants.dart';
 
 class HomeScreen extends StatelessWidget {
-   @override
+  final items = List<String>.generate(10000, (i) => 'Item $i');
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Swipe Cards"),
+        backgroundColor: kPrimaryColor,
       ),
       body: SafeArea(
-        child: Container(
-          padding: EdgeInsets.fromLTRB(10,10,10,0),
-          height: 220,
-          width: double.maxFinite,
-          child: Card(
-            elevation: 5,
-          ),
+        child: ListView.builder(
+          itemCount: items.length,
+          itemBuilder: (context, index) {
+            return Card(
+              elevation: 3,
+              child: ListTile(
+                title: Text(items[index]),
+                trailing: Icon(Icons.arrow_right),
+              ),
+              margin: EdgeInsets.fromLTRB(16, 8, 16, 0),
+              shape: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  borderSide: BorderSide(color: kPrimaryColor, width: 1)),
+            );
+          },
         ),
       ),
     );
