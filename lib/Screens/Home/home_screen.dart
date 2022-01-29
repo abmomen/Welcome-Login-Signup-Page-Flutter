a10ab5795446eb7e3bf504dd9ebd3ae38ccd89f8
Abdul Momen <abdulmomen130@gmail.com>
2022-01-29 22:54:09 +0600

    added onclick listener

--- lib/Screens/Home/home_screen.dart
+++ lib/Screens/Home/home_screen.dart
@@ -17,41 +17,47 @@ class HomeScreen extends StatelessWidget {
           itemBuilder: (context, index) {
             return Card(
               elevation: 3,
-              child: Column(
-                children: [
-                  Container(
-                    child: Align(
-                      alignment: Alignment.centerLeft,
-                      child: Text(
-                      "Word List: " + (index + 1).toString(),
-                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22),
+              child: InkWell(
+                splashColor: kPrimaryLightColor,
+                onTap: () {
+                  debugPrint('Card tapped.');
+                },
+                child: Column(
+                  children: [
+                    Container(
+                      child: Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                        "Word List: " + (index + 1).toString(),
+                        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22),
+                        ),
                       ),
-                    ),
-                    margin: EdgeInsets.fromLTRB(16, 16, 16, 0),
-                    ),
-                  Container(
-                    child: Align(
-                      alignment: Alignment.centerLeft,
-                      child: Text(
-                      "7 Mastered out of 10",
-                      style: TextStyle(fontWeight: FontWeight.normal, fontSize: 18),
+                      margin: EdgeInsets.fromLTRB(16, 16, 16, 0),
                       ),
-                    ),
-                    margin: EdgeInsets.fromLTRB(16, 10, 16, 0)
-                    ),
-                  Container(
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.all(Radius.circular(10)),
-                      child: LinearProgressIndicator(
-                        backgroundColor: kPrimaryLightColor,
-                        color: kPrimaryColor,
-                        value: 0.70,
-                        minHeight: 20,
+                    Container(
+                      child: Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                        "7 Mastered out of 10",
+                        style: TextStyle(fontWeight: FontWeight.normal, fontSize: 18),
+                        ),
                       ),
+                      margin: EdgeInsets.fromLTRB(16, 10, 16, 0)
+                      ),
+                    Container(
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                        child: LinearProgressIndicator(
+                          backgroundColor: kPrimaryLightColor,
+                          color: kPrimaryColor,
+                          value: 0.70,
+                          minHeight: 20,
+                        ),
+                      ),
+                      margin: const EdgeInsets.fromLTRB(16, 8, 16, 16),
                     ),
-                    margin: const EdgeInsets.fromLTRB(16, 8, 16, 16),
-                  ),
-                ],
+                  ],
+                ),
               ),
               margin: EdgeInsets.fromLTRB(16, 8, 16, 0),
               shape: OutlineInputBorder(
